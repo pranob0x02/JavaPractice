4dfa9771e479507073166a6831f88a26016d81f4
pranob0x02 <pranob.kanti.nath@eek.ee>
2023-01-07 14:11:03 +0200

    Yield statement

--- src/SwitchTest.java
+++ src/SwitchTest.java
@@ -10,5 +10,28 @@ public class SwitchTest {
             default -> System.out.println("was neither");
         }
 
+        System.out.println(getQuarter("January"));
+
     }
+
+    public static String getQuarter(String month) {
+        return switch (month) {
+            case "January", "February", "March" -> {
+                yield "1st";
+                //Switch statement is being used as a switch expression returning a value.
+                //Case label uses a code block, with opeining and closingn curly braces.
+            }
+            case "April", "May", "June" -> "2nd";
+            case "July", "August", "September" -> "3rd";
+            case "October", "November", "Decmeber" -> "4rd";
+            default -> {
+                String badResponse = "404 NOT FOUND " + month;
+                yield badResponse;
+            }
+        };
+
+
+    }
+
+
 }
