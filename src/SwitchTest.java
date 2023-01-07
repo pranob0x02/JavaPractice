8214ce0a58be108f027ed99da542bc2fedcf4be8
pranob0x02 <pranob.kanti.nath@eek.ee>
2023-01-07 13:54:35 +0200

    NewSwitch statement

--- src/SwitchTest.java
+++ src/SwitchTest.java
@@ -2,20 +2,12 @@ public class SwitchTest {
     public static void main(String[] args) {
         int switchValue = 3;
         switch (switchValue) {
-            case 1:
-                System.out.println("value was 1");
-                break;
-            case 2:
-                System.out.println("value was 2");
-                break;
-            case 3:
-            case 4:
-            case 5:
-                System.out.println("the value was " + switchValue);
-                break;
-            default:
-                System.out.println("was neither");
-                break;
+            case 1 -> System.out.println("value was 1");
+
+            case 2 -> System.out.println("value was 2");
+
+            case 3, 4, 5 -> System.out.println("the value was " + switchValue);
+            default -> System.out.println("was neither");
         }
 
     }
