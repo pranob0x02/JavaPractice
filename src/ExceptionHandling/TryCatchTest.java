6fc0039a0094f8141aa95ceacd106f871b2da4b9
pranob0x02 <pranob.kanti.nath@eek.ee>
2023-01-30 01:52:04 +0200

    how to take UserInput and Try-Catch basic overview

---
+++ src/ExceptionHandling/TryCatchTest.java
@@ -0,0 +1,19 @@
+package ExceptionHandling;
+
+public class TryCatchTest {
+    public static void main(String[] args) {
+
+    }
+}
+
+
+
+/*
+ *   try {
+ *       // statements that might get errors
+ *   } catch (Exception e){
+ *       // code to 'handle the exception'
+ *   }
+ *
+ *
+ * */
---
+++ src/ReadingUserInput/UserInputTest.java
@@ -0,0 +1,49 @@
+package ReadingUserInput;
+
+import java.util.Scanner;
+
+public class UserInputTest {
+    public static void main(String[] args) {
+        int currentYear = 2023;
+
+        try {
+            System.out.println(getInputFromConsole(currentYear));
+        } catch (NullPointerException e) {
+            System.out.println(getInputFromScanner(currentYear));
+        }
+    }
+
+
+    public static String getInputFromConsole(int currentYear) {
+
+        String name = System.console().readLine("Hi, what's your name? ");
+        System.out.println("Welcome " + name);
+
+        String dateOfBirth = System.console().readLine("What year were you born? ");
+        int age = currentYear - Integer.parseInt(dateOfBirth);
+        return "So, you are " + age + " years old.";
+    }
+
+
+    public static String getInputFromScanner(int currentYear) {
+
+        /*
+         *      ---------------- Instantiating Scanner ----------------
+         *           Scanner s = new Scanner(System.in)
+         *
+         *       --------------- For reading input from a file -----------
+         *           Scanner s = new Scanner (new File("nameOfFile"));
+         *
+         * */
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hi, what's your name?");
+        String name = scanner.nextLine();
+        System.out.println("Welcome " + name);
+
+        System.out.println("What year were you born? ");
+        String dateOfBirth = scanner.nextLine();
+        int age = currentYear - Integer.parseInt(dateOfBirth);
+        return "So, you are " + age + " years old.";
+    }
+}
